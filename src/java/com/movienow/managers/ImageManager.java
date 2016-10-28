963f74d3cf804dc4898c297358c904ad830e439b
tunaonder <saito@vt.edu>
2016-10-27 23:40:34 -0400

    movie detail page is added, galeria is added

---
+++ src/java/com/movienow/managers/ImageManager.java
@@ -0,0 +1,24 @@
+package com.movienow.managers;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImageManager {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            images.add("movie" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
--- src/java/com/movienow/managers/MovieManager.java
+++ src/java/com/movienow/managers/MovieManager.java
@@ -5,6 +5,7 @@
  */
 package com.movienow.managers;
 
+import com.movienow.entityclasses.Movie;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Serializable;
